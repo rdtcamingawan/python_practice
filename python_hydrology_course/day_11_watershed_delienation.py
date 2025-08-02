aa45a499fdbb81bc74aff94e5bd4bfd464895acd
rdtcamingawan <127808722+rdtcamingawan@users.noreply.github.com>
2025-08-02 16:01:27 +0800

    feat: add watershed delineation with whitebox

---
+++ python_hydrology_course/day_11_watershed_delienation.py
@@ -0,0 +1,89 @@
+# Import modules
+import rasterio
+import math
+import whitebox_workflows as wbw
+from whitebox_workflows import show
+import geopandas as gpd
+from matplotlib import pyplot as plt
+
+# Initialize WhiteboxTools environment
+wbe = wbw.WbEnvironment()
+
+# File paths - local
+dem_path = r"C:\Users\richmond\Downloads\testing\srtm.tif"
+point_save_path = r"C:\Users\richmond\Downloads\testing\shps\pour_point.shp"
+fig_save_path = r"C:\Users\richmond\Downloads\testing\watershed_delineation.png"
+
+# Read raster
+dem = wbe.read_raster(dem_path)
+
+# Create a pour point
+point_geom = gpd.points_from_xy(x=[76.04], y=[12.91], crs='EPSG:32643')
+point = gpd.GeoDataFrame(geometry=point_geom)
+point.to_file(point_save_path, driver='ESRI ShapeFile')
+
+# =======================================================================
+# Perform hydrologic functions
+# =======================================================================
+
+
+# 1. Depressionless DEM
+dem_no_deps = wbe.fill_depressions(dem, flat_increment=0.001)
+
+# 2. Flow accumulation
+channel_threshold = 25
+flow_accum = wbe.qin_flow_accumulation(dem_no_deps, 
+                                       out_type='cells',
+                                       convergence_threshold=channel_threshold, 
+                                       log_transform=True
+                                       )
+wbe.write_raster(flow_accum,
+                 r"C:\Users\richmond\Downloads\testing\wbt_flow_accum.tif")
+
+# 3. Map the streams by thresholding
+streams = flow_accum > math.log(channel_threshold)
+
+# 4. Snap point to streams
+point = wbe.read_vector(point_save_path)
+point = wbe.jenson_snap_pour_points(point, streams, 5.0)
+
+# 5. D8 Pointer
+d8_pntr = wbe.d8_pointer(dem_no_deps)
+
+# 6. Extract watershed
+watershed = wbe.watershed(d8_pointer=d8_pntr, pour_points=point)
+watershed_vec = wbe.raster_to_vector_polygons(watershed) # vectorize
+watershed_vec = wbe.smooth_vectors(watershed_vec, filter_size=5)  # smooths
+wbe.write_vector(watershed_vec, 'watershed.gpkg') # save to file
+
+# 7. Extract streams inside the watershed
+streams = streams * watershed
+streams_vec = wbe.raster_streams_to_vector(streams, d8_pntr)
+streams_vec, tmp1, tmp2, tmp3 = wbe.vector_stream_network_analysis(streams_vec, dem_no_deps)
+# wbe.write_vector(streams_vec, 'streams.gpkg') # save to file
+
+# Extract streams based on Strahler order
+strahler_streams = wbe.strahler_stream_order(d8_pntr=d8_pntr, 
+                                             streams_raster=streams)
+strahler_streams_vec = wbe.raster_streams_to_vector(strahler_streams, d8_pntr)
+
+
+
+fig, ax = plt.subplots()
+
+ax = show(dem, ax=ax, label = 'DEM')
+ax = show(point, ax=ax, label= 'outlet')
+ax = show(watershed_vec, ax=ax, label= 'watershed')
+ax = show(strahler_streams_vec, ax=ax, label='stream network')
+
+ax.set_xlabel('long')
+ax.set_ylabel('lat')
+
+plt.title('Watershed Delineation')
+plt.legend()
+plt.tight_layout()
+# plt.savefig(fig_save_path)
+
+plt.show()
+
+
